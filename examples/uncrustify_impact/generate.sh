68af8b2dc5dc7f8b0cbd5d847d64834efedf4f12
anetczuk <anetczuk@o2.pl>
2024-08-02 21:54:17 +0200

    Update example

--- examples/uncrustify_impact/generate.sh
+++ examples/uncrustify_impact/generate.sh
@@ -18,11 +18,4 @@ $SCRIPT_DIR/../../src/uncrustifyimpact.py impact --file $SCRIPT_DIR/example.cpp 
 												 --config $SCRIPT_DIR/config.cfg \
 												 --outputdir $OUT_DIR \
 												 --randomseed 0 \
-												 --ignoreparams cmt_width cmt_cpp_to_c nl_max code_width indent_cmt_with_tabs \
-												 						  indent_columns sp_before_tr_emb_cmt sp_before_semi \
-												 						  nl_before_func_body_def nl_func_type_name \
-												 						  indent_sing_line_comments indent_braces \
-												 						  sp_cmt_cpp_start nl_remove_extra_newlines \
-												 						  nl_return_expr pos_arith \
-												 						  nl_end_of_file
-
+												 --ignoreparams code_width cmt_width indent_columns
